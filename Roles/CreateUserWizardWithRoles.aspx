7d7bf7f57f128ce6ff14c2c221ee52514c6561ad
Drew Baker <drew.baker999@gmail.com>
2016-05-11 01:44:26 -0400

    Create user wizard with roles
    creating new users wizard with wizard step for specifying what roles the
    new user should be added to.

--- Roles/CreateUserWizardWithRoles.aspx
+++ Roles/CreateUserWizardWithRoles.aspx
@@ -1,6 +1,15 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="CreateUserWizardWithRoles.aspx.cs" Inherits="Roles_CreateUserWizardWithRoles" %>
+﻿<%@ Page Title="Create User Wizard With Roles" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="CreateUserWizardWithRoles.aspx.cs" Inherits="Roles_CreateUserWizardWithRoles" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <asp:CreateUserWizard ID="RegisterUserWithRoles" runat="server" ContinueDestinationPageUrl="~/Default.aspx" LoginCreatedUser="False" OnActiveStepChanged="RegisterUserWithRoles_ActiveStepChanged">
+        <WizardSteps>
+            <asp:CreateUserWizardStep runat="server" />
+            <asp:WizardStep ID="SpecifyRolesStep" runat="server" AllowReturn="False" StepType="Step" Title="Specify Roles">
+                <asp:CheckBoxList runat="server" ID="RoleList"></asp:CheckBoxList>
+            </asp:WizardStep>
+            <asp:CompleteWizardStep runat="server" />
+        </WizardSteps>
+    </asp:CreateUserWizard>
 </asp:Content>
 
 
--- Roles/CreateUserWizardWithRoles.aspx.cs
+++ Roles/CreateUserWizardWithRoles.aspx.cs
@@ -4,11 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class Roles_CreateUserWizardWithRoles : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+        {
+            // Reference the SpecifyRolesStep WizardStep
+            WizardStep SpecifyRolesStep = RegisterUserWithRoles.FindControl("SpecifyRolesStep") as WizardStep;
 
+            // Reference the RoleList CheckBoxList
+            CheckBoxList RoleList = SpecifyRolesStep.FindControl("RoleList") as CheckBoxList;
+
+            // Bind the set of roles to RoleList
+            RoleList.DataSource = Roles.GetAllRoles();
+            RoleList.DataBind();
+        }
+    }
+    protected void RegisterUserWithRoles_ActiveStepChanged(object sender, EventArgs e)
+    {
+        if (RegisterUserWithRoles.ActiveStep.Title == "Complete")
+        {
+            // Reference the SpecifyRolesStep WizardStep
+            WizardStep SpecifyRolesStep = RegisterUserWithRoles.FindControl("SpecifyRolesStep") as WizardStep;
+
+            // Reference the RoleList CheckBoxList
+            CheckBoxList RoleList = SpecifyRolesStep.FindControl("RoleList") as CheckBoxList;
+
+            // Add the checked roles to the newly created user
+            foreach (ListItem li in RoleList.Items)
+            {
+                if (li.Selected)
+                    Roles.AddUserToRole(RegisterUserWithRoles.UserName, li.Text);
+            }
+        }
     }
 }
